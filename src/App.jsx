839660d3037e0737348c51f653b2eb84eccd12bd
LDM <ldemestre@hotmail.com>
2026-01-09 09:14:42 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from './pages/Dashboard'
 import OperationView from './pages/OperationView'
 import NewOperation from './pages/NewOperation'
 import Metrics from './pages/Metrics'
+import BattleMapPage from './pages/BattleMapPage'
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/operation/new" element={<NewOperation />} />
         <Route path="/operation/:id" element={<OperationView />} />
         <Route path="/metrics" element={<Metrics />} />
+        <Route path="/battlemap" element={<BattleMapPage />} />
       </Routes>
     </Layout>
   )
