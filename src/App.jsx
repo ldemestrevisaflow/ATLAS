51cd73e28fc15a54638410dafd12d76cd0dce0e8
LDM <ldemestre@hotmail.com>
2026-01-08 15:49:30 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import OperationView from './pages/OperationView'
 import NewOperation from './pages/NewOperation'
+import Metrics from './pages/Metrics'
 
-function App() {
+export default function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/operation/new" element={<NewOperation />} />
         <Route path="/operation/:id" element={<OperationView />} />
+        <Route path="/metrics" element={<Metrics />} />
       </Routes>
     </Layout>
   )
 }
-
-export default App
